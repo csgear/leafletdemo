3a932356eeb2c744c22c666958cd4a14716b8d97
Xiaojun Yang <csgear@live.com>
2021-08-20 15:29:18 -0400

    a working demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./App.css";
 import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
 
 function App() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ width: "80%", height: "80vh" }}
+    >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
